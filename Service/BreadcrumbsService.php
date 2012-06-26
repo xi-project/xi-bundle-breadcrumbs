1c6d908706022132cbcbbcdca32539a52081060d
Peter Hillerström <peter.hillerstrom@soprano.fi>
2012-06-26 14:43:54 +0300

    OY-546: Started writing the service using routes and routers with fixtures.

--- Service/BreadcrumbsService.php
+++ Service/BreadcrumbsService.php
@@ -2,9 +2,11 @@
 
 namespace Xi\Bundle\BreadcrumbsBundle\Service;
 
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
-
+use \Symfony\Component\DependencyInjection\ContainerInterface;
+use \Symfony\Component\HttpKernel\Event\FilterControllerEvent;
+use \Symfony\Component\Routing\Route;
+use \Symfony\Component\Routing\RouteCollection;
+use \Symfony\Component\Routing\RouterInterface;
 
 /**
  * A service class to build breadcrumbs from Symfony routes.
@@ -19,9 +21,95 @@ class BreadcrumbsService
      */
     protected $container;
 
+    /**
+     * @var RouterInterface
+     */
+    protected $router;
+
+    protected $logger;
+
     public function __construct(ContainerInterface $container)
     {
         $this->container = $container;
+        $this->router = $container->get('router');
+        $this->logger = $container->get('logger');
+
+    }
+
+    public function getRouter()
+    {
+        return $this->router;
+    }
+
+    public function setRouter(RouterInterface $router)
+    {
+        return $this->router = $router;
+    }
+
+    /**
+     * @param string $route
+     *
+     * @return array
+     */
+    public function getBreadcrumbs($route, RouteCollection $rc = null, array $params = array())
+    {
+        if ("string" === gettype($route)) {
+            $url = $this->getUri($route, $params);
+            $this->logger->debug(
+                "The URL for '${route}' is ${url}."
+            );
+
+            $route = $this->getRouteByName($route, $rc);
+        }
+
+        if ($route && $route->hasDefault('label') && $route->hasDefault('parent')) {
+            return array_merge(
+                (array) $this->getParents($route),
+                (array) $route->getDefault('label')
+            );
+        } else {
+            if ($route->hasDefault('parent')) {
+                $this->logger->warn(
+                    'Please specify both label and parent for route: ' . $route->getPattern()
+                );
+            }
+            return array();
+        }
+    }
+
+    /**
+     * @param Route|null $route
+     *
+     * @return array
+     */
+    public function getParents($route)
+    {
+        if ($route && $route->hasDefault('parent')) {
+            $parent = $route->getDefault('parent');
+            return array_merge(
+                (array) $this->getParents($this->getRouteByName($parent)),
+                (array) $parent
+            );
+        } else {
+            return array();
+        }
+    }
+
+    private function getUri($route, array $params = array()) {
+        return $this->router->generate($route, $params);
+    }
+
+    private function getRouteByName($name, RouteCollection $rc = null)
+    {
+        if (!$rc) { $rc = $this->getRoutes(); }
+
+        $route = $rc->get($name) or array();
+        return $route;
+    }
+
+    private function getRoutes()
+    {
+        return $this->getRouter()->getRouteCollection();
     }
 
 }
--- Tests/Service/BreadcrumbsServiceTest.php
+++ Tests/Service/BreadcrumbsServiceTest.php
@@ -5,13 +5,21 @@ namespace Xi\Bundle\BreadcrumbsBundle\Tests\Service;
 use \Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use \Symfony\Component\EventDispatcher\EventDispatcher;
 use \Symfony\Component\EventDispatcher\Event;
+use \Symfony\Component\Config\FileLocator;
 use \Symfony\Component\DomCrawler\Crawler;
+use \Symfony\Component\Routing\Loader\YamlFileLoader;
+use \Symfony\Component\Routing\Route;
+use \Symfony\Component\Routing\Router;
+use \Symfony\Component\Routing\RouterInterface;
+use \Symfony\Component\Routing\RouteCollection;
 use \Xi\Bundle\BreadcrumbsBundle\Tests\ContainerTestCase; // @TODO how to reuse and not copy here?
 use \Xi\Bundle\BreadcrumbsBundle\Service\BreadcrumbsService;
 
 
 /**
  * @group service
+ *
+ * @author Peter Hillerström <peter.hillerstrom@soprano.fi>
  */
 class BreadcrumbsServiceTest extends ContainerTestCase
 {
@@ -32,7 +40,10 @@ class BreadcrumbsServiceTest extends ContainerTestCase
      * @group service
      */
     public function testConstructor() {
-        $this->assertInstanceOf('Xi\Bundle\BreadcrumbsBundle\Service\BreadcrumbsService', new BreadcrumbsService($this->getContainer()));
+        $this->assertInstanceOf(
+            'Xi\Bundle\BreadcrumbsBundle\Service\BreadcrumbsService',
+            new BreadcrumbsService($this->getContainer())
+        );
     }
 
     /**
@@ -40,6 +51,101 @@ class BreadcrumbsServiceTest extends ContainerTestCase
      * @group service
      */
     public function testGetServiceFromContainer() {
-        $this->assertInstanceOf('Xi\Bundle\BreadcrumbsBundle\Service\BreadcrumbsService', $this->getContainer()->get('xi_breadcrumbs'));
+        $this->assertInstanceOf(
+            'Xi\Bundle\BreadcrumbsBundle\Service\BreadcrumbsService',
+            $this->getContainer()->get('xi_breadcrumbs')
+        );
+    }
+
+    /**
+     * @test
+     * @group service
+     */
+    public function testHasRouterAndRouteCollection()
+    {
+        $router = $this->service->getRouter();
+        $this->assertInstanceOf('Symfony\Component\Routing\Router', $router);
+
+        $rc = $router->getRouteCollection();
+        $this->assertInstanceOf('Symfony\Component\Routing\RouteCollection', $rc);
+    }
+
+    /**
+     * @test
+     * @group service
+     */
+    public function testGetBreadcrumbsForRouteWithoutParent()
+    {
+        $route = new Route('/home', array('label' => 'ignored'));
+        $this->assertEquals(array(), $this->service->getBreadcrumbs($route));
+    }
+
+    /**
+     * @test
+     * @group service
+     */
+    public function testGetBreadcrumbsForRouteWithoutLabel()
+    {
+        $route = new Route('/home', array('parent' => 'ignored'));
+        $this->assertEquals(array(), $this->service->getBreadcrumbs($route));
+    }
+
+    /**
+     * @test
+     * @group service
+     */
+    public function testGetBreadcrumbsForRouteWithParent()
+    {
+        $this->service->setRouter($this->loadRouter('oneparent.yml'));
+
+        $breadcrumbs = array('root', 'foo');
+        $this->assertEquals($breadcrumbs, $this->service->getBreadcrumbs('foo'));
+    }
+
+    /**
+     * @test
+     * @group service
+     */
+    public function testGetBreadcrumbsForRouteWithParams()
+    {
+        $this->service->setRouter($this->loadRouter('oneparent.yml'));
+
+        $breadcrumbs = array('root', 'foo', 'bar {slug}');
+
+        $this->assertEquals(
+            $breadcrumbs,
+            $this->service->getBreadcrumbs(
+                'bar',
+                null,
+                array('slug' => 'b1-1')
+            )
+        );
+    }
+
+    /**
+     * @param string @yamlFile YAML routing configuration file name
+     * @return RouteCollection
+     */
+    private function loadRouteCollection($yamlFile)
+    {
+        $loader = new YamlFileLoader(new FileLocator(array(__DIR__.'/../Fixtures')));
+        return $loader->load($yamlFile);
+    }
+
+    /**
+     * @param string @yamlFile YAML routing configuration file name
+     * @return Router
+     */
+    private function loadRouter($yamlFile)
+    {
+        $locator = new FileLocator(array(__DIR__.'/../Fixtures'));
+        /* $requestContext = new RequestContext($_SERVER['REQUEST_URI']); */
+
+        return new Router(
+            new YamlFileLoader($locator),
+            $yamlFile /*,
+            array('cache_dir' => __DIR__.'/cache'),
+            $requestContext */
+        );
     }
 }
